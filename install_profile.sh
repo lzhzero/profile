9371849d986f8f0e606b095a0656f1dd5aae22f9
zliu <zliu@dtranx-1.c6220-16.rdma-pg0.apt.emulab.net>
2018-09-05 14:38:53 -0600

    new procedure

--- install_profile.sh
+++ install_profile.sh
@@ -3,9 +3,9 @@ cp -r .tmux* ~/
 
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p ~/.local/share/fonts
-mkdir -p ~/.config/fontconfig/conf.d/
-m PowerlineSymbols.otf ~/.local/share/fonts/
-sudo apt install fontconfig
-fc-cache -vf ~/.local/share/fonts/
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+sudo apt update
+sudo apt install fonts-powerline cmake python-dev
+git clone https://github.com/VundleVim/Vundle.vim ./.vim/bundle/Vundle.vim
+vim +PluginInstall +qall +silent
+~/.vim/bundle/YouCompleteMe/install.py --all 
+
