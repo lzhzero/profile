38208e4f415c40a60aa5be648553ae1b0e4bd107
zliu <zhang.liu87@gmail.com>
2018-10-02 10:12:50 -0600

    update install_profile.sh for automation

--- install_profile.sh
+++ install_profile.sh
@@ -1,10 +1,8 @@
 cp -r .vim* ~/
 cp -r .tmux* ~/
 
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 sudo apt update
-sudo apt install fonts-powerline cmake python-dev
+sudo apt install fonts-powerline cmake python-dev -y
 git clone https://github.com/VundleVim/Vundle.vim ./.vim/bundle/Vundle.vim
 vim +PluginInstall +qall +silent
 ~/.vim/bundle/YouCompleteMe/install.py --all 
--- tmux_install.sh
+++ tmux_install.sh
@@ -4,5 +4,5 @@ wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 tar zxvf tmux-2.6.tar.gz
 cd tmux-2.6
 ./configure
-make -j10
+make -j
 sudo make install
