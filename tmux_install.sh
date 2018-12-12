d241daa7a382eb36d3651aec86a639d17edd3a7d
zliu <zhang.liu87@gmail.com>
2018-12-12 11:28:05 -0500

    apt update

--- tmux_install.sh
+++ tmux_install.sh
@@ -1,4 +1,5 @@
 git config --global http.sslVerify false
+sudo apt -y update
 sudo apt -y install libevent-dev ncurses-dev vim
 wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 tar zxvf tmux-2.6.tar.gz
