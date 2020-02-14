9261140395de196dc51d7d677eaef9b67039cbec
zliu <zhang.liu87@gmail.com>
2020-02-13 23:21:40 -0700

    update tmux.conf remove paring for sharing bug

--- tmux_install.sh
+++ tmux_install.sh
@@ -1,9 +1,9 @@
 git config --global http.sslVerify false
 sudo apt -y update
 sudo apt -y install libevent-dev ncurses-dev vim
-wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
-tar zxvf tmux-2.6.tar.gz
-cd tmux-2.6
+wget https://github.com/tmux/tmux/releases/download/3.0/tmux-3.0.tar.gz
+tar zxvf tmux-3.0.tar.gz
+cd tmux-3.0
 ./configure
 make -j
 sudo make install
